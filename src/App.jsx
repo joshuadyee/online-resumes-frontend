023529488f43e6b7dcc48bcb7b7f24f36f98a768
ebanchik <ebanchik@gmail.com>
2024-01-30 16:18:39 -0500

    adding backend data

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar"
 import { LandingPage } from "./components/LandingPage"
 import { Cards } from "./components/Cards"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
+  const resumes = []
   return (
     <div className="App">
-      <NavBar/>
-      <LandingPage />
-      <Cards />
+      <BrowserRouter>
+        <NavBar />
+        <LandingPage /> {/* Landing Page Section */}
+        <Cards resumes={resumes} /> {/* Cards Section */}
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+/* <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/cards" element={<Cards resumes={resumes} />} />
+        // </Routes>
+      // </BrowserRouter> */
