ca222612ce5ffc861dc8a9c354b911ca3c6849b7
Erika Gonzalez <erikagonzz01@gmail.com>
2024-01-31 12:09:56 -0500

    added all students profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Profile from "./Profile";
+import Profile from "./Profile";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { Home } from "./Home"
@@ -40,6 +40,7 @@ function App() {
           {/* <Route path="/students" element={<StudentDetails/>} /> */}
           <Route path="/students" element={<StudentsIndex students={students} />} />
           <Route path="/students/:id" element={<StudentDetails/>} />
+          <Route path="/profile/:studentId" element={<Profile />} />
        
         
         </Routes>
